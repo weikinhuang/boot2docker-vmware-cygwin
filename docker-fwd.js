4134298bafa2c61e95d10335854c23f6c0061104
Wei Kin Huang <weikin.huang04@gmail.com>
2015-02-06 15:21:18 -0500

    WIP port forwarding server

--- docker-fwd.js
+++ docker-fwd.js
@@ -1,55 +1,53 @@
 "use strict";
 
 var child_process = require("child_process"),
-	activeTunnel = null,
-	activePorts = [];
-
+    activeTunnel = null,
+    activePorts = [];
 function killActiveTunnel() {
-	if (activeTunnel) {
-		activeTunnel.kill("SIGINT");
-		activeTunnel = null;
-	}
+    if (activeTunnel) {
+        activeTunnel.kill("SIGINT");
+        activeTunnel = null;
+    }
 }
-
 function hasDiff(arr1, arr2) {
-	return arr1.filter(function(i) {return !(arr2.indexOf(i) > -1);}).length > 0 ||
-		arr2.filter(function(i) {return !(arr1.indexOf(i) > -1);}).length > 0;
+    return arr1.filter(function(i) { return !(arr2.indexOf(i) > -1); }).length > 0 ||
+        arr2.filter(function(i) { return !(arr1.indexOf(i) > -1); }).length > 0;
 }
-
 function updatePorts() {
-	child_process.exec("bash docker forwarded-ports", function(err, stdout, stderr) {
-		if (stderr) {
-			process.stderr.write(stderr);
-		}
-		if (err) {
-			return;
-		}
-		var ports = [];
-		stdout.replace(/\d+\/\w+ -> \d+.\d+.\d+.\d+:(\d+)/g, function(m, p) {
-			ports.push(+p);
-		});
-		if (!hasDiff(activePorts, ports)) {
-			return;
-		}
-		killActiveTunnel();
-		var args = [
-			"-N",
-			"-o", "UserKnownHostsFile=/dev/null",
-			"-o", "StrictHostKeyChecking=no",
-			"-o", "ConnectTimeout=2"
-		];
-		ports.forEach(function(p) {
-			args.push("-L", p + ":" + process.env.BOOT2DOCKER_HOST + ":" + p);
-		});
-		args.push("docker@" + process.env.BOOT2DOCKER_HOST);
-		activeTunnel = child_process.spawn("ssh", args);
-		activePorts = ports;
-	});
+    child_process.exec("bash docker forwarded-ports", function(err, stdout, stderr) {
+        if (stderr) {
+            process.stderr.write(stderr);
+        }
+        if (err) {
+            return;
+        }
+        var ports = [];
+        stdout.replace(/\d+\/\w+ -> \d+.\d+.\d+.\d+:(\d+)/g, function(m, p) {
+            ports.push(+p);
+        });
+        if (!hasDiff(activePorts, ports)) {
+            return;
+        }
+        killActiveTunnel();
+        var args = [
+            "-N",
+            "-o", "UserKnownHostsFile=/dev/null",
+            "-o", "StrictHostKeyChecking=no",
+            "-o", "ConnectTimeout=2"
+        ];
+        ports.forEach(function(p) {
+            args.push("-L", p + ":" + process.env.BOOT2DOCKER_HOST + ":" + p);
+        });
+        args.push("docker@" + process.env.BOOT2DOCKER_HOST);
+        activeTunnel = child_process.spawn("ssh", args);
+        activePorts = ports;
+    });
 }
-
 setTimeout(updatePorts, 5000);
 process.on("exit", killActiveTunnel);
-require("http").createServer(function(request, response) {
-	setTimeout(updatePorts, 5000);
-	response.end("");
-}).listen(59145, "localhost");
+require("http")
+    .createServer(function(request, response) {
+        setTimeout(updatePorts, 5000);
+        response.end("");
+    })
+    .listen(59145, "localhost");
